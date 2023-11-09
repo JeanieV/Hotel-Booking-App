1289871501a2a5caf75c8be929d5a169050e72a2
JeanieV <115072704+JeanieV@users.noreply.github.com>
2023-11-09 17:46:06 +0200

    update 4

--- api/index.php
+++ api/index.php
@@ -2,14 +2,19 @@
 
 header('Content-Type: application/json');
 
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Start a session here if needed
+    session_start();
+
+    // Include the functions file
+    require './functions.php';
+    require './index.php';
     $response = array(
-        'message'=> 'Hello, World'
+        'message' => 'Hello, World'
     );
 
     echo json_encode($response);
-}
-else{
+} else {
     http_response_code(405);
     echo json_encode(array('error' => 'Method Not Allowed'));
 }
